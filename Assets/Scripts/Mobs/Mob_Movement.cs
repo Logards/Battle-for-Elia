cad277d75a594be2fd406208870223776cc48e89
LeoM <54435611+Tenlutshy@users.noreply.github.com>
2024-01-15 11:29:06 +0100

    Mobs Movement

--- Assets/Scripts/Mobs/Mob_Movement.cs
+++ Assets/Scripts/Mobs/Mob_Movement.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Mob_Movement : MonoBehaviour
 {
     private GameObject target;
-    public float speed = 0.2f;
+    public float speed = 1f;
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(target.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(target.transform.position * Time.deltaTime * speed);
+        transform.Translate((target.transform.position - transform.position) * Time.deltaTime * speed);
     }
 }
